53e43b22b4d156924b18714aaee53bb07c91b44f
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2023-01-14 22:27:52 -0500

    Update bible_verse.dart

--- lib/routes/bible_verse.dart
+++ lib/routes/bible_verse.dart
@@ -1,3 +1,5 @@
+import 'package:adventist_pocket/models/verse.dart';
+import 'package:adventist_pocket/services/bible_api_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,14 +11,35 @@ class BibleVerse extends ConsumerStatefulWidget {
 }
 
 class _BibleVerseState extends ConsumerState<BibleVerse> {
+  final ScrollController _scrollController = ScrollController();
+  List<Verse> _verses = [];
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      _scrollController.jumpTo(1000);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Verses'),
       ),
-      body: const Center(
-        child: Text('Verses'),
+      body: ListView.builder(
+        controller: _scrollController,
+        itemCount: 100,
+        itemBuilder: (context, index) {
+          print(index);
+          // BibleApiService().getVerse('PSA', '119', '${index + 1}').then((verse) {
+          //   print(verse[0].content);
+          // });
+          return ListTile(
+            title: Text('Verse $index'),
+          );
+        },
       ),
     );
   }
